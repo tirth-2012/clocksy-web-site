7237f61352e57564dfff3f6bf24384d4a0814776
tirth-2012 <tirthbhoraniya2012@gmail.com>
2025-04-26 16:53:18 +0530

    setting static

--- clocksy/settings.py
+++ clocksy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-mp(o3zc=tt5$_ri9b19f*-!^*ry9bb*g$%v3pg0)rne@*ixf$0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False   
 
-ALLOWED_HOSTS = ["yourdomain.com", "www.yourdomain.com", "your-server-ip"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
