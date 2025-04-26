41c75bf6534e1a835bee4f0b0828d1732be257e8
tirth-2012 <tirthbhoraniya2012@gmail.com>
2025-04-26 16:38:52 +0530

    setting static

--- clocksy/settings.py
+++ clocksy/settings.py
@@ -126,11 +126,20 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static')
 # ]
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
