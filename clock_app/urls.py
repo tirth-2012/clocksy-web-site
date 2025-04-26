95e01fa5c02b7d5f8105e16d5b7c7c9426e09e67
tirth-2012 <tirthbhoraniya2012@gmail.com>
2025-04-26 08:59:24 +0530

    urls

--- clock_app/urls.py
+++ clock_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -21,6 +23,6 @@ urlpatterns = [
     path("account/", views.account, name="account"),
     
 
-] 
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
